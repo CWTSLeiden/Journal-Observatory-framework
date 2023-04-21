76f5227d0904374556adb8d527e99ee3e07f3ea7
Bram van den Boomen <bram.boomen@gmail.com>
2023-04-21 23:57:05 +0200

    fix ontology inconsistencies

--- web/wicodo-generate.sh
+++ web/wicodo-generate.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# https://dgarijo.github.io/Widoco/doc/bestPractices/index-en.html
+# https://github.com/dgarijo/Widoco/blob/master/doc/metadataGuide/guide.md
+
+# https://github.com/dgarijo/Widoco#options
+
 java -jar java-17-widoco-1.4.17-jar-with-dependencies.jar \
      -ontFile ../scpo_ontology.ttl \
      -outFolder scpo \
