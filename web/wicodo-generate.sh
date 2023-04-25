36dea977a2417fbe96efc9a2311828351ccbc67f
Bram van den Boomen <bram.boomen@gmail.com>
2023-04-25 10:43:54 +0200

    update ontology documentation

--- web/wicodo-generate.sh
+++ web/wicodo-generate.sh
@@ -9,14 +9,14 @@ java -jar java-17-widoco-1.4.17-jar-with-dependencies.jar \
      -ontFile ../scpo_ontology.ttl \
      -outFolder scpo \
      -rewriteAll \
-     -includeAnnotationProperties \
      -getOntologyMetadata \
+     -noPlaceHolderText \
      -uniteSections
 
 java -jar java-17-widoco-1.4.17-jar-with-dependencies.jar \
      -ontFile ../pad_framework.ttl \
      -outFolder pad \
      -rewriteAll \
-     -includeAnnotationProperties \
      -getOntologyMetadata \
+     -noPlaceHolderText \
      -uniteSections
